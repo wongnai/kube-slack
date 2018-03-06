f96bed09060efe569e3a74cf80ccb945ac840a77
Manatsawin Hanmongkolchai <manatsawin@wongnai.com>
2018-03-06 09:15:22 +0700

    Added fallback text when pod ready status is not set

--- src/monitors/longnotready.js
+++ src/monitors/longnotready.js
@@ -2,13 +2,13 @@ const EventEmitter = require('events');
 const config = require('config');
 const kube = require('../kube');
 
-class PodLongNotReady extends EventEmitter{
-	constructor(){
+class PodLongNotReady extends EventEmitter {
+	constructor() {
 		super();
 		this.minimumTime = config.get('not_ready_min_time');
 	}
 
-	start(){
+	start() {
 		setInterval(() => {
 			this.check();
 		}, config.get('interval'));
@@ -16,54 +16,65 @@ class PodLongNotReady extends EventEmitter{
 		return this;
 	}
 
-	async check(){
+	async check() {
 		let pods = await kube.getPods();
 
-		for(let pod of pods){
-
+		for (let pod of pods) {
 			// Ignore pod if the annotation is set and evaluates to true
-			if(pod.metadata.annotations && pod.metadata.annotations['kube-slack/ignore-pod']){
+			if (
+				pod.metadata.annotations &&
+				pod.metadata.annotations['kube-slack/ignore-pod']
+			) {
 				continue;
 			}
 
-			if(!pod.status || !pod.status.conditions){
+			if (!pod.status || !pod.status.conditions) {
 				continue;
 			}
 
-			let readyStatus = pod.status.conditions.filter((item) => item.type === 'Ready');
-			
-			if(readyStatus.length === 0){
+			let readyStatus = pod.status.conditions.filter(
+				item => item.type === 'Ready'
+			);
+
+			if (readyStatus.length === 0) {
 				continue;
 			}
 
 			readyStatus = readyStatus[0];
 
-			if(readyStatus.status === 'True'){
+			if (readyStatus.status === 'True') {
 				continue;
 			}
-			
-			if(readyStatus.reason === 'PodCompleted'){
+
+			if (readyStatus.reason === 'PodCompleted') {
 				continue;
 			}
 
 			let notReadySince = new Date(readyStatus.lastTransitionTime).getTime();
 			let notReadyDuration = new Date().getTime() - notReadySince;
 
-			if(notReadyDuration < this.minimumTime){
+			if (notReadyDuration < this.minimumTime) {
 				continue;
 			}
 
 			let key = pod.metadata.name;
 
-			if(pod.metadata.ownerReferences && pod.metadata.ownerReferences.length > 0){
+			if (
+				pod.metadata.ownerReferences &&
+				pod.metadata.ownerReferences.length > 0
+			) {
 				key = pod.metadata.ownerReferences[0].name;
 			}
 
 			this.emit('message', {
-				fallback: `Pod ${pod.metadata.namespace}/${pod.metadata.name} is not ready: ${readyStatus.reason} - ${readyStatus.message}`,
+				fallback: `Pod ${pod.metadata.namespace}/${
+					pod.metadata.name
+				} is not ready: ${readyStatus.reason} - ${readyStatus.message}`,
 				color: 'danger',
-				title: `${pod.metadata.namespace}/${pod.metadata.name}: ${readyStatus.reason}`,
-				text: readyStatus.message,
+				title: `${pod.metadata.namespace}/${
+					pod.metadata.name
+				}: ${readyStatus.reason || 'Pod not ready'}`,
+				text: readyStatus.message || 'Pod not ready',
 				_key: key,
 			});
 		}
