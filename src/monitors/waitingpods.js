a22826bbee24d88b16876fde6813f5196eec5db9
Manatsawin Hanmongkolchai <manatsawin@wongnai.com>
2018-03-29 11:42:35 +0700

    Revert "Added ContainersNotReady to blacklist reason"
    This reverts commit afbd9699d7e9e94cb495cc0f6cde5cb544f1c9d4.

--- src/monitors/waitingpods.js
+++ src/monitors/waitingpods.js
@@ -2,17 +2,13 @@ const EventEmitter = require('events');
 const config = require('config');
 const kube = require('../kube');
 
-class PodStatus extends EventEmitter {
-	constructor() {
+class PodStatus extends EventEmitter{
+	constructor(){
 		super();
-		this.blacklistReason = [
-			'ContainerCreating',
-			'PodInitializing',
-			'ContainersNotReady',
-		];
+		this.blacklistReason = ['ContainerCreating', 'PodInitializing'];
 	}
 
-	start() {
+	start(){
 		setInterval(() => {
 			this.check();
 		}, config.get('interval'));
@@ -20,47 +16,34 @@ class PodStatus extends EventEmitter {
 		return this;
 	}
 
-	async check() {
+	async check(){
 		let containers = await kube.getContainerStatuses();
 
-		for (let item of containers) {
+		for(let item of containers){
+
 			// Ignore pod if the annotation is set and evaluates to true
-			if (
-				item.pod.metadata.annotations &&
-				item.pod.metadata.annotations['kube-slack/ignore-pod']
-			) {
+			if(item.pod.metadata.annotations && item.pod.metadata.annotations['kube-slack/ignore-pod']){
 				continue;
 			}
 
-			if (!item.state.waiting) {
+			if(!item.state.waiting){
 				continue;
 			}
-			if (this.blacklistReason.includes(item.state.waiting.reason)) {
+			if(this.blacklistReason.includes(item.state.waiting.reason)){
 				continue;
 			}
 
 			let key = item.pod.metadata.name;
 
-			if (
-				item.pod.metadata.ownerReferences &&
-				item.pod.metadata.ownerReferences.length > 0
-			) {
+			if(item.pod.metadata.ownerReferences && item.pod.metadata.ownerReferences.length > 0){
 				key = item.pod.metadata.ownerReferences[0].name;
 			}
 
 			this.emit('message', {
-				fallback: `Container ${item.pod.metadata.namespace}/${
-					item.pod.metadata.name
-				}/${item.name} entered status ${item.state.waiting.reason} (${
-					item.state.waiting.message
-				})`,
+				fallback: `Container ${item.pod.metadata.namespace}/${item.pod.metadata.name}/${item.name} entered status ${item.state.waiting.reason} (${item.state.waiting.message})`,
 				color: 'danger',
-				title: `${item.pod.metadata.namespace}/${item.pod.metadata.name}/${
-					item.name
-				}`,
-				text: `Container entered status *${item.state.waiting.reason}*\n\`\`\`${
-					item.state.waiting.message
-				}\`\`\``,
+				title: `${item.pod.metadata.namespace}/${item.pod.metadata.name}/${item.name}`,
+				text: `Container entered status *${item.state.waiting.reason}*\n\`\`\`${item.state.waiting.message}\`\`\``,
 				mrkdwn_in: ['text'],
 				_key: key,
 			});
